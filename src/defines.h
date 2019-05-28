9618b8e8e179f643da1d7705355007fd64ee03fc
Maike Mota <maike_henrique@hotmail.com>
2019-05-27 23:32:02 -0300

    Definições de pino de sensor ultrason, ajustes pinos motores

--- src/defines.h
+++ src/defines.h
@@ -1,9 +1,17 @@
-#define MOTOR_ESQUERDO_PWM 9
-#define MOTOR_ESQUERDO_IN1 7
-#define MOTOR_ESQUERDO_IN2 8
+#define MOTOR_ESQUERDO_PWM 3
+#define MOTOR_ESQUERDO_IN1 2
+#define MOTOR_ESQUERDO_IN2 4
 
-#define MOTOR_DIREITO_PWM 3
-#define MOTOR_DIREITO_IN1 2
-#define MOTOR_DIREITO_IN2 4
+#define MOTOR_DIREITO_PWM 9
+#define MOTOR_DIREITO_IN1 7
+#define MOTOR_DIREITO_IN2 8
 
-#define LOG(content) Serial.println(content)
+#define TRIGGER_PIN 12
+#define ECHO_PIN 13
+
+#define DELAY_VIRAR 750
+#define DELAY_RE 500
+
+#define DEBUG false
+
+#define LOG(content) Serial.print(content)
