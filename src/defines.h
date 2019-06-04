8bd03b4f5dcade7d45ee1a75590e195211ab140f
Maike Mota <maike_henrique@hotmail.com>
2019-06-04 01:11:20 -0300

    Definidas novas contanstes

--- src/defines.h
+++ src/defines.h
@@ -1,17 +1,26 @@
 #define MOTOR_ESQUERDO_PWM 3
-#define MOTOR_ESQUERDO_IN1 2
-#define MOTOR_ESQUERDO_IN2 4
+#define MOTOR_ESQUERDO_IN1 4
+#define MOTOR_ESQUERDO_IN2 5
 
-#define MOTOR_DIREITO_PWM 9
-#define MOTOR_DIREITO_IN1 7
-#define MOTOR_DIREITO_IN2 8
+#define MOTOR_DIREITO_PWM 11
+#define MOTOR_DIREITO_IN1 12
+#define MOTOR_DIREITO_IN2 13
 
-#define TRIGGER_PIN 12
-#define ECHO_PIN 13
+#define SENSOR_LINHA_CENTRAL 2
+#define SENSOR_LINHA_ESQUERDA 9
+#define SENSOR_LINHA_DIREITA 8
 
-#define DELAY_VIRAR 750
-#define DELAY_RE 500
+#define TRIGGER_PIN 6
+#define ECHO_PIN 7
 
-#define DEBUG false
+#define DISTANCIA_MINIMA_PERSEGUIR 25
+#define TEMPO_FUGIR_LINHA 150
+#define TEMPO_VIRADA_PROCURAR_OPONENTE 250
 
-#define LOG(content) Serial.print(content)
+// #define DEBUG
+
+#ifdef DEBUG
+    #define LOG(prefixo, valor, sufixo) Serial.print(prefixo); Serial.print(valor); Serial.print(sufixo)
+    #define QUEBRA_LINHA() Serial.println();
+    #define LINE_FEED \n
+#endif
